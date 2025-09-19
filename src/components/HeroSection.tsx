7769b6bdba8401d949d64c8c630b0b75e5090048
Hemanth-kumar-009 <polavaramhemanthkumar@gmail.com>
2025-09-19 11:54:16 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -104,7 +104,7 @@ const HeroSection = () => {
           </div>
 
           {/* Social Links */}
-          
+          {/*
           <div
             className={`flex justify-center space-x-6 transition-all duration-1000 delay-1100 ${
               isVisible
@@ -113,9 +113,9 @@ const HeroSection = () => {
             }`}
           >
             {[
-              { /*icon: Github, label: "GitHub", href: "#" */},
-              {/* icon: Linkedin, label: "LinkedIn", href: "#"*/ },
-              {/* icon: Mail, label: "Email", href: "#"*/ },
+              {icon: Github, label: "GitHub", href: "#" },
+              { icon: Linkedin, label: "LinkedIn", href: "#" },
+              { icon: Mail, label: "Email", href: "#" },
             ].map(({ icon: Icon, label, href }) => (
               <a
                 key={label}
@@ -126,6 +126,7 @@ const HeroSection = () => {
                 <Icon className="w-5 h-5" />
               </a>
             ))}
+            */}
           </div>
         </div>
       </div>
